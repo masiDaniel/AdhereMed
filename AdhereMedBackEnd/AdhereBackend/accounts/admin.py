66bed5659bbd8ad574a255bf9f9e9ae335f6c541
masiDaniel <masidaniel02@gmail.com>
2025-10-29 11:29:50 +0300

    chore(deletion): deleted the mobile codebase - will start  afresh after designing

--- AdhereMedBackEnd/AdhereBackend/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from accounts.models import CustomUser, Doctor, Patient
-
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Doctor)
-admin.site.register(Patient)
--- AdhereMedFrontEnd/version_0/android/app/src/main/kotlin/com/example/version_0/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.version_0
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
